b87ae67e7bb74a43e9b559c7936b3954b3315139
sm00k3y <kubasmolka1999@gmail.com>
2021-08-28 12:17:01 +0200

    Almost finished backend - Initial Commit

---
+++ app.js
@@ -0,0 +1,32 @@
+// import express from 'express';
+const express = require("express");
+const bodyParser = require("body-parser");
+const { graphqlHTTP } = require("express-graphql");
+const mongoose = require("mongoose");
+
+const graphqlSchema = require("./graphql/schema/index");
+const graphqlResolvers = require("./graphql/resolvers/index");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: graphqlSchema,
+    rootValue: graphqlResolvers,
+    graphiql: true,
+  })
+);
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.646yw.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
